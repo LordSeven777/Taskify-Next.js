a9991883f7a8b3b39abc76518bd63f922526151a
Tolotra <tolotor@yahoo.fr>
2023-06-02 11:12:16 +0300

    Update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}", // Note the addition of the `app` directory.
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/features/**/*.{js,ts,jsx,tsx,mdx}",
 
     // Or if using `src` directory:
     // "./src/**/*.{js,ts,jsx,tsx,mdx}",
